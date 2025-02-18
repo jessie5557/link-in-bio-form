12e1a142bcdb25d1cdafdf45883d80b05dcf05c9
jessie5557 <ychen38@chicagobooth.edu>
2025-02-18 03:29:13 +0000

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,23 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def form
+    render({:template => "item_templates/form"})
+  
+  end
+
+  def add
+    
+    i = Item.new
+
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+
+    i.save
+
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", {:controller => "items", :action => "form"})
+
+  post("/add", {:controller => "items", :action => "add"})
 end
